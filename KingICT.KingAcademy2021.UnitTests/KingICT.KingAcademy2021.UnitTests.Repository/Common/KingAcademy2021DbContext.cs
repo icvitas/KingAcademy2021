5f3edeb1622c056c290aac561d8e6938e88de965
Ivan Cvitas <ivan.cvitas@king-ict.hr>
2021-07-19 11:50:07 +0200

    add ctor to db context

--- KingICT.KingAcademy2021.UnitTests/KingICT.KingAcademy2021.UnitTests.Repository/Common/KingAcademy2021DbContext.cs
+++ KingICT.KingAcademy2021.UnitTests/KingICT.KingAcademy2021.UnitTests.Repository/Common/KingAcademy2021DbContext.cs
@@ -4,5 +4,7 @@ namespace KingICT.KingAcademy2021.UnitTests.Repository.Common
 {
     public class KingAcademy2021DbContext : DbContext
     {
+        public KingAcademy2021DbContext(DbContextOptions<KingAcademy2021DbContext> options)
+            : base(options) { }
     }
 }
